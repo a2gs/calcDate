1b019820ce3d4c05b3ec2c9ec9f1fd636e1fd71c
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2019-03-03 01:18:56 -0300

    Update libAddSubDate.c

--- src/libAddSubDate.c
+++ src/libAddSubDate.c
@@ -1,5 +1,42 @@
+/* Andre Augusto Giannotti Scota (a2gs)                                                    
+ * andre.scota@gmail.com
+ *
+ * Lib calcDate
+ *
+ * Public Domain
+ *
+ */
+
+
+/* libAddSubDate.c
+ * Lib do add/subtract date.
+ *
+ *  Who     | When       | What
+ *  --------+------------+----------------------------
+ *   a2gs   | 03/03/2019 | Creation
+ *          |            |
+ */
+
+
+/* *** INCLUDES ************************************************************************ */
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#include "libAddSubDate.h"
+
+
+/* *** DEFINES AND LOCAL DATA TYPE DEFINATION ****************************************** */
+
+
+/* *** LOCAL PROTOTYPES (if applicable) ************************************************ */
+
+
+/* *** EXTERNS / LOCAL / GLOBALS VARIEBLES ********************************************* */
+
+
 /*
- *      struct tm {
+        struct tm {
          int tm_sec;     * Seconds (0-60) * 
          int tm_min;     * Minutes (0-59) * 
          int tm_hour;    * Hours (0-23) * 
@@ -12,33 +49,31 @@
      };
 
  The members of the tm structure are:
-
  tm_sec    The number of seconds after the minute, normally in the range 0 to 59, but can be up to 60 to allow for leap seconds.
-
  tm_min    The number of minutes after the hour, in the range 0 to 59.
-
  tm_hour   The number of hours past midnight, in the range 0 to 23.
-
  tm_mday   The day of the month, in the range 1 to 31.
-
  tm_mon    The number of months since January, in the range 0 to 11.
-
  tm_year   The number of years since 1900.
-
  tm_wday   The number of days since Sunday, in the range 0 to 6.
-
  tm_yday   The number of days since January 1, in the range 0 to 365.
-
  tm_isdst  A flag that indicates whether daylight saving time is in effect at the time described.  The value is positive if daylight saving time is in effect, zero if it is not, and negative if the information is not available.
 */
 
-
-#include <stdio.h>
-#include <string.h>
-#include <time.h>
-
-#include "libAddSubDate.h"
-
+/* *** FUNCTIONS *********************************************************************** */
+/* int a2gs_DateCalculation(struct tm *dataStart, a2gs_AddSubOperation_e op, long delta, struct tm *dataEnd)
+ *
+ *
+ *
+ * INPUT:
+ *  dataStart -
+ *  op -
+ *  delta -
+ * OUTPUT:
+ *  dataEnd -
+ *  0 -
+ *  -1 -
+ */
 int a2gs_DateCalculation(struct tm *dataStart, a2gs_AddSubOperation_e op, long delta, struct tm *dataEnd)
 {
 	struct tm *dataOut;
