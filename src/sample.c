02b2511687f745a53b32dd5f0a7ae9d540f314eb
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-03-05 14:32:31 -0300

    Adding A2GS_ADDSUBDATE_DAY_TO_SECONDS() feature in sample

--- src/sample.c
+++ src/sample.c
@@ -46,12 +46,13 @@ int main(int argc, char *argv[])
 	int min = 0;
 	int seg = 0;
 	unsigned long delta = 0;
+	char typeDelta = 0;
 	a2gs_AddSubOperation_e op;
 	struct tm endDate;
 	int ret = 0;
 
-	if(argc != 9){
-		printf("%s <DIA> <MES> <ANO> <HORA> <MINUTO> <SEGUNDO> [+-] <DELTA SEGUNDOS>\n", argv[0]);
+	if(argc != 10){
+		printf("%s <DAY> <MONTH> <YEAR> <HOURS> <MINUTES> <SECONDS> [+-] <S|D> <DELTA (seconds|days)>\n", argv[0]);
 		return(0);
 	}
 
@@ -63,16 +64,24 @@ int main(int argc, char *argv[])
 	min  = atoi(argv[5]);
 	seg  = atoi(argv[6]);
 
-	delta = atol(argv[8]);
-
 	if     (argv[7][0] == '+') op = ADD;
 	else if(argv[7][0] == '-') op = SUB;
 	else{
-		printf("Only '+' or '-' operations.\n");
+		printf("Only '+' or '-' as operation types.\n");
 		return(0);
 	}
 
-	/* ret = a2gs_AddSubDate(dia, mes, ano, hora, min, seg, op, A2GS_ADDSUBDATE_DAY_TO_SECONDS(delta_in_days), &endDate); */
+	if      (argv[8][0] == 'D' || argv[8][0] == 'd'){
+		typeDelta = 'D';
+		delta = A2GS_ADDSUBDATE_DAY_TO_SECONDS(atol(argv[9]));
+	}else if(argv[8][0] == 'S' || argv[8][0] == 's'){
+		typeDelta = 'S';
+		delta = atol(argv[9]);
+	}else{
+		printf("Only 'S|s' or 'D|d' as delta types.\n");
+		return(0);
+	}
+
 	ret = a2gs_AddSubDate(dia, mes, ano, hora, min, seg, op, delta, &endDate);
 
 	if(ret == -1){
@@ -80,9 +89,9 @@ int main(int argc, char *argv[])
 		return(-1);
 	}
 
-	printf("%02d/%02d/%02d %02d:%02d:%02d %c %ld secs = %02d/%s (%02d)/%02d %02d:%02d:%02d, Week: %s (%d), Day in the year: %d, Leap year: %s, Daylight saving time: %d\n",
+	printf("%02d/%02d/%02d %02d:%02d:%02d %c %s %s = %02d/%s (%02d)/%02d %02d:%02d:%02d, Week: %s (%d), Day in the year: %d, Leap year: %s, Daylight saving time: %d\n",
 	       dia, mes, ano, hora, min, seg,
-	       argv[7][0], delta,
+	       argv[7][0], argv[9], (typeDelta == 'S' ? "sec(s)" : "day(s)"),
 	       endDate.tm_mday, A2GS_ADDSUBDATE_N_MONTH_TO_MONTH_STR(endDate.tm_mon), A2GS_ADDSUBDATE_MONTH(endDate.tm_mon), A2GS_ADDSUBDATE_YEAR(endDate.tm_year),
 	       endDate.tm_hour, endDate.tm_min, endDate.tm_sec, A2GS_ADDSUBDATE_N_DAY_TO_WEEK_STR(endDate.tm_wday), A2GS_ADDSUBDATE_WEEK(endDate.tm_wday),
 	       endDate.tm_yday, (a2gs_AddSubDate_isLeapYear(A2GS_ADDSUBDATE_YEAR(endDate.tm_year)) ? "yes" : "no"), endDate.tm_isdst);
